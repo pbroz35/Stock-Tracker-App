12dbb9b390cc816408cc4cc299a12840d168bcfa
Piotr Brozek <1brozek.peter@gmail.com>
2024-03-17 16:36:18 -0500

    set up yahoo finance api

---
+++ app.py
@@ -0,0 +1,16 @@
+import yfinance as yf
+from flask import request, render_template, jsonify, Flask
+
+app = Flask(__name__, template_folder= 'templates')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_stock_data', methods=['POST'])
+def get_stock_data():
+    ticker = request.get_json()['ticker']
+    data = yf.Ticker(ticker).history(period='1y')
+    return jsonify({'currentPrice': data.iloc[-1].Close, #close price and open price to compare if it went up/down
+                      'openPrice': data.iloc[-1].Open })
